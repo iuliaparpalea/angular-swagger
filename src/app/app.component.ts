b9e9d678208304c5ef7feda63bb5ffd3cff5ed7c
Iulia Parpalea <iuliaparpalea@gmail.com>
2019-05-02 11:55:14 +0300

    Configure webpack  * start app: npm run webpack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+declare const VERSION: string;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'angular-swagger';
+  title = 'Version: ' + VERSION;
 }
